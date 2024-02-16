048739cd9d630010c2f79f825ec146192bc92623
arghjain29 <arghjain1234@gmail.com>
2024-02-17 01:06:33 +0530

    Final changes

--- Binary search.cpp
+++ Binary search.cpp
@@ -1,42 +1,38 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int num, st, mid, end, tgt;
+#include <iostream>  
+using namespace std;  
+int main ()  
+{  
+    
+    int arr[10] = {23,34,42,48,58,76,77,86,89,92};
+    int low, mid, end, tgt;
 
-    cout << "Enter the number of element in array: ";
-    cin >> num;
-    int arr[num];
+    low = 0;  
+    end = sizeof(arr) - 1; 
+      
+ 
+    cout << " Define a value to be searched from sorted array: " << endl;  
+    cin >> tgt;  
+      
+    while ( low <= end)  
+    {  
 
-    cout << "Enter the Number in Ordered form (inc/dec) ==> " << endl;
-    for (int i = 0; i < num; i++)
-    {
-        cin >> arr[i];
-    }
-
-    st = 0;
-    end = num - 1;
-    cout << " Define a value to be searched from sorted array: " << endl;
-    cin >> tgt;
-
-    while (st <= end)
-    {
-        mid = (st + end) / 2;
-
-        if (arr[mid] == tgt)
-        {
-            cout << " Element is found at index " << (mid);
-            exit(0);
-        }
-        else if (tgt > arr[mid])
-        {
-            st = mid + 1;
-        }
-        else if (tgt < arr[mid])
-        {
-            end = mid - 1;
-        }
-    }
-    cout << " Number is not found. " << endl;
+        mid = ( low + end ) / 2;  
+  
+        if (arr[mid] == tgt)  
+        {  
+            cout << " Element is found at index " << (mid);  
+            exit (0);   
+        }  
+        else if ( tgt > arr[mid])  
+        {  
+            low = mid + 1; 
+        }  
+          
+        else if ( tgt < arr[mid])  
+        {  
+            end = mid - 1;  
+        }  
+    }  
+    cout << " Number is not found. " << endl;  
     return 0;
 }
